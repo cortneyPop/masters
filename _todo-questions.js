74e1eba04ad1005d5e8383b566bed61c38c9b8b3
Cortney Rasmussen <cortney@opticpop.com>
2015-08-18 16:20:27 -0500

    initial

---
+++ _todo-questions.js
@@ -0,0 +1,31 @@
+// figure out how to get working on phone - live updates!
+
+
+
+
+// TOOLS
+
+// stylus
+// webpack
+// yeti.css
+// gatekeeper github https://github.com/prose/gatekeeper
+// storing the github secret (gatekeeper)
+// heroku - run a aserver for oauth
+// on github in your profile in settings in developer - create your own app for oauth and
+
+
+What we are building:
+http://hubtags.com/
+
+
+READ:
+// javascript-allonge leanpub 
+
+to deploy:
+surge sh (npm tool)
+
+..create a deploy script
+
+
+
+
